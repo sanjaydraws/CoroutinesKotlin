15faba12dd75afb3a1accb79f3e712f93515a33c
sanjaydraws <sprajapat8331@gmail.com>
2021-10-11 23:57:26 +0530

    intermediate flow operators

--- asynchronousflow/Program6.kt
+++ asynchronousflow/Program6.kt
@@ -1,2 +1,14 @@
 // flow can be transformed with operators , just as you would with collection and sequences 
-Intermediate operators are applied to an upstream flow and return a downstream flow. These operators are cold, just like flows are. A
+// Intermediate operators are applied to an upstream flow and return a downstream flow. These operators are cold, just like flows are. A
+
+fun main() = runBlocking<Unit>{
+    (1..3).asFlow()// a flow of request
+    .map{request -> performRequest(request)}
+    .collect{response -> println(response)}
+    
+}
+
+suspend fun performRequest(request:Int):String{
+    delay(100) // imitate long-running asynchronous task 
+    return "response $request"
+}
